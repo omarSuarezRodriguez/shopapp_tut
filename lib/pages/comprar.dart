b4711c3561841204d5eb9c826ea450d532d69e69
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2020-01-13 17:44:25 -0500

    commit

--- lib/pages/comprar.dart
+++ lib/pages/comprar.dart
@@ -5,7 +5,6 @@
 
 //import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 //import 'package:flutter_ui_challenges/core/presentation/res/assets.dart';
 //import 'package:flutter_ui_challenges/src/widgets/network_image.dart';
