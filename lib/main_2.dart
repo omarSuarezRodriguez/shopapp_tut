caaca179c2a8662c575cb8b687d8cdd9207cb785
omarSuarezRodriguez <51038495+omarSuarezRodriguez@users.noreply.github.com>
2020-01-12 23:11:02 -0500

    Initial commit

---
+++ lib/main_2.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'dart:async';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration(seconds: 3),
+      () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          'images/slider.png',
+          height: 400,
+          width: 400,
+        ),
+      ),
+    );
+  }
+}
